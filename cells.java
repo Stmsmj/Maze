6a8b7345e22d91d1100d853b5efb0c5644d0eb9b
amirhoprogrammer <108186011+amirhoprogrammer@users.noreply.github.com>
2022-06-30 19:03:28 +0430

    Add files via upload

--- cells.java
+++ cells.java
@@ -1,10 +1,15 @@
-package AliHube.com;
-
+package com.company;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class cells {
 
+    // باید بدم به نگارشی کل اینو بدون تغییر
     public int row,r,col,c,Cell_Size;
     public boolean[] Walls = {true,true,true,true};
     public ArrayList<cells> Next;
@@ -27,6 +32,66 @@ public class cells {
         Font_Weight = (int)(10+0.3*(Cell_Size-20));
         Stroke_Size = (int)((5.0/9 * ((Cell_Size/10)-1) )+1);
 
+    }
+    public void Draw_Cell(Graphics g) {
+        Graphics2D G2D = (Graphics2D) g;
+
+        G2D.setColor(new Color(0,0,0));
+        G2D.setStroke(new BasicStroke(Stroke_Size));
+
+        if(Walls[0] == true)
+            G2D.drawLine(c, r, c+Cell_Size, r);
+        if(Walls[1] == true)
+            G2D.drawLine(c+Cell_Size, r, c+Cell_Size, r+Cell_Size);
+        if(Walls[2] == true)
+            G2D.drawLine(c+Cell_Size, r+Cell_Size, c, r+Cell_Size);
+        if(Walls[3] == true)
+            G2D.drawLine(c, r+Cell_Size, c, r);
+
+        G2D.setStroke(new BasicStroke(1));
+    }
+
+    public void drawWeight(Graphics g) {
+        g.setFont(new Font("",Font.BOLD,Font_Weight));
+        g.drawString(""+Weight, (int)(c+Cell_Size/2)-(int)(Font_Weight/3), (int)(r+Cell_Size/2)+(int)(Font_Weight/3));
+
+    }
+
+    public void Draw_Box(Graphics g,Color color) {
+        Graphics2D G2D = (Graphics2D) g;
+        if(this.Visited == true) {
+            G2D.setColor(color);
+            G2D.fillRect(c, r, Cell_Size, Cell_Size);
+        }
+    }
+
+    public void drawPath(Graphics g,Color color) {
+        Graphics2D G2D = (Graphics2D) g;
+        G2D.setColor(color);
+        if(Parent!=null) {
+            if(this.Visited_Path == true) {
+                G2D.fillRect(c+(int)(Cell_Size/3), r+(int)(Cell_Size/3), (int)(Cell_Size/3), (int)(Cell_Size/3));
+            }
+            if(row - 1 == Parent.row) {
+                G2D.fillRect(c+(int)(Cell_Size/3), r, (int)(Cell_Size/3), (int)(Cell_Size/3));
+                G2D.fillRect(Parent.c+(int)(Cell_Size/3), Parent.r+((int)(Cell_Size/3)*2), (int)(Cell_Size/3), (int)(Cell_Size/3));
+            }
+
+            if(col + 1 == Parent.col) {
+                G2D.fillRect(c+((int)(Cell_Size/3)*2), r+(int)(Cell_Size/3), (int)(Cell_Size/3), (int)(Cell_Size/3));
+                G2D.fillRect(Parent.c, Parent.r+(int)(Cell_Size/3), (int)(Cell_Size/3), (int)(Cell_Size/3));
+            }
+
+            if(row + 1 == Parent.row) {
+                G2D.fillRect(c+(int)(Cell_Size/3), r+((int)(Cell_Size/3)*2), (int)(Cell_Size/3), (int)(Cell_Size/3));
+                G2D.fillRect(Parent.c+(int)(Cell_Size/3), Parent.r, (int)(Cell_Size/3), (int)(Cell_Size/3));
+            }
+
+            if(col - 1 == Parent.col) {
+                G2D.fillRect(c, r+(int)(Cell_Size/3), (int)(Cell_Size/3), (int)(Cell_Size/3));
+                G2D.fillRect(Parent.c+((int)(Cell_Size/3)*2), Parent.r+(int)(Cell_Size/3), (int)(Cell_Size/3), (int)(Cell_Size/3));
+            }
+        }
     }
 
 
