49fb68e8fe7e62ebf7e12bf6af6355fcdff0bf2f
Stmsmj <96914984+Stmsmj@users.noreply.github.com>
2022-07-11 16:26:34 +0430

    Update myMaze.java
    fixing again:/

--- myMaze.java
+++ myMaze.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.util.*;
 
@@ -52,7 +51,7 @@ public class myMaze{
     public void drawMaze(Graphics g) {
         for(int i = 0;i<w;i++) {
             for(int j = 0;j<w;j++) {
-                grids[i][j].Draw_Box(g, new Color(110,2060,200,2500));
+                grids[i][j].Draw_Box(g, new Color(110,206,200,250));
                 grids[i][j].Draw_Cell(g);
             }
         }
@@ -71,13 +70,13 @@ public class myMaze{
     }
 
     public void drawPathFinder(Graphics g,int mode) {
-        if(mode == 0)
+        if(mode==0){
             play(g);
+        }
         else if(mode == 1)
             mazeFinderBFS(g);
-        else if(mode==2){
+        else if(mode == 2)
             mazeFinderDFS(g);
-        }
 
         for(cells x: Paths_From_A_to_B) {
             x.drawPath(g, Color.WHITE);
@@ -115,6 +114,8 @@ public class myMaze{
             }
         }
     }
+
+
 
     public void update() {
         if (running == false)
@@ -166,6 +167,7 @@ public class myMaze{
             }
         }
     }
+
     public void play(Graphics g) {
         myCode=myFrame.getCode();
         Next = move(myCode,current);
@@ -189,6 +191,8 @@ public class myMaze{
         }
 
     }
+
+
     public void mazeFinderDFS(Graphics g) {
 
         Next = getOneNeighbor(current);
@@ -215,6 +219,7 @@ public class myMaze{
         }
     }
 
+
     private void Path_Start_End() {
         Paths_From_A_to_B.add(End);
         cells tempParent = End.Parent;
@@ -233,7 +238,9 @@ public class myMaze{
             A.Next.add(B);
     }
 
+
     public void initStartAndEnd() {
+
 
         Start = grids[new Random().nextInt(w)][new Random().nextInt(w)];
         End = grids[new Random().nextInt(w)][new Random().nextInt(w)];
@@ -258,6 +265,7 @@ public class myMaze{
         Next = current;
 
     }
+
     public cells move(int keyCode,cells currentCell){
         cells myCell=null;
         if(keyCode==87){
@@ -283,44 +291,60 @@ public class myMaze{
         return  myCell;
 
     }
+
+
     public cells getOneNeighbor(cells currentCell){
         ArrayList<cells> neighbors = new ArrayList<>();
+
         if (currentCell.row-1 >= 0 && grids[currentCell.row-1][currentCell.col].Visited_Path == false && currentCell.Walls[0] == false) {
             neighbors.add(grids[currentCell.row-1][currentCell.col]);
         }
+
         if (currentCell.col+1 < w && grids[currentCell.row][currentCell.col+1].Visited_Path == false && currentCell.Walls[1] == false) {
             neighbors.add(grids[currentCell.row][currentCell.col+1]);
         }
+
         if (currentCell.row + 1 < w && grids[currentCell.row+1][currentCell.col].Visited_Path == false && currentCell.Walls[2] == false) {
             neighbors.add(grids[currentCell.row+1][currentCell.col]);
         }
+
         if (currentCell.col-1 >= 0 && grids[currentCell.row][currentCell.col-1].Visited_Path == false && currentCell.Walls[3] == false) {
             neighbors.add(grids[currentCell.row][currentCell.col-1]);
         }
+
+
         if (neighbors.isEmpty())
             return null;
+
         return  neighbors.get(0);
     }
 
     public cells getOneRandomNeighbor(cells currentCell) {
         ArrayList<cells> neighbors = new ArrayList<>();
+
         if (currentCell.row-1 >= 0 && grids[currentCell.row-1][currentCell.col].Visited == false) {
             neighbors.add(grids[currentCell.row-1][currentCell.col]);
         }
+
         if (currentCell.col+1 < w && grids[currentCell.row][currentCell.col+1].Visited == false) {
             neighbors.add(grids[currentCell.row][currentCell.col+1]);
         }
+
         if (currentCell.row + 1 < w && grids[currentCell.row+1][currentCell.col].Visited == false) {
             neighbors.add(grids[currentCell.row+1][currentCell.col]);
         }
+
         if (currentCell.col-1 >= 0 && grids[currentCell.row][currentCell.col-1].Visited == false) {
             neighbors.add(grids[currentCell.row][currentCell.col-1]);
         }
+
+
         if (neighbors.isEmpty())
             return null;
-        return  neighbors.get(new Random().nextInt(neighbors.size()));
-    }
 
+        return  neighbors.get(new Random().nextInt(neighbors.size()));
+
+    }
     public boolean hasNeighbor(cells currentCell) {
         if (getOneRandomNeighbor(currentCell)!=null)
             return true;
@@ -360,7 +384,7 @@ public class myMaze{
     }
 
     public void setCellSize(int cellSize) {
-        Cell_Size = cellSize + 400;
+        Cell_Size = cellSize;
     }
 
 
